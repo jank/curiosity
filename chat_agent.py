d158eee1bfe0da891493c9c0ab382e4bc8f620ff
jank <jan@familie-karstens.net>
2024-08-08 15:48:13 +0200

    Black code formatting

--- chat_agent.py
+++ chat_agent.py
@@ -10,14 +10,16 @@ load_dotenv()
 agents = {}
 checkpointer = None
 
-def get_checkpoint(thread_id:str):
+
+def get_checkpoint(thread_id: str):
     config = {"configurable": {"thread_id": thread_id}}
     global checkpointer
     if checkpointer is None:
         checkpointer = SqliteSaver.from_conn_string("data/curiosity.db")
     return checkpointer.get(config)
 
-def get_agent(model_id:str):
+
+def get_agent(model_id: str):
     global agents
     if not model_id in agents:
         search = TavilySearchResults(max_results=3)
@@ -27,9 +29,13 @@ def get_agent(model_id:str):
         if model_id == "gpt-4o-mini":
             model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
         elif model_id == "llama3.1":
-            model = ChatOpenAI(model="llama3.1", base_url="http://localhost:11434/v1", temperature=0)
+            model = ChatOpenAI(
+                model="llama3.1", base_url="http://localhost:11434/v1", temperature=0
+            )
         elif model_id == "llama3-groq-8b-8192-tool-use-preview":
-            model = ChatGroq(model="llama3-groq-8b-8192-tool-use-preview", temperature=0)
+            model = ChatGroq(
+                model="llama3-groq-8b-8192-tool-use-preview", temperature=0
+            )
         else:
             raise Exception(f"Model not supported: {model_id}")
         agent = create_react_agent(model, tools, checkpointer=cp)
--- curiosity.py
+++ curiosity.py
@@ -17,23 +17,31 @@ import asyncio
 # datamodel
 # (user) 1-n> (chats) 0-n> (cards / stored in LangGraph db)
 
-db = database('data/curiosity.db')
+db = database("data/curiosity.db")
 chats = db.t.chats
 if chats not in db.t:
-    chats.create(id=str, title=str, updated=datetime, pk='id')
+    chats.create(id=str, title=str, updated=datetime, pk="id")
 ChatDTO = chats.dataclass()
+
 
 # Patch ChatDTO class with ft renderer and ID initialization
 @patch
-def __ft__(self:ChatDTO): # type: ignore
-    return A(textwrap.shorten(self.title, width=60, placeholder="..."), id=self.id, href=f'/chat/{self.id}')
+def __ft__(self: ChatDTO):  # type: ignore
+    return A(
+        textwrap.shorten(self.title, width=60, placeholder="..."),
+        id=self.id,
+        href=f"/chat/{self.id}",
+    )
+
 
 @patch
-def __post_init__(self:ChatDTO): # type: ignore
+def __post_init__(self: ChatDTO):  # type: ignore
     self.id = shortuuid.uuid()
+
 
 new_chatDTO = ChatDTO()
 new_chatDTO.id = shortuuid.uuid()
+
 
 @dataclass
 class ChatCard:
@@ -41,25 +49,32 @@ class ChatCard:
     content: str
     busy: bool = False
     sources: List = None
-    id: str=''
+    id: str = ""
 
     def __post_init__(self):
         self.id = shortuuid.uuid()
 
     def __ft__(self):
         return Card(
-            Progress() if self.busy else P(self.content, cls="markdown"), 
-            header=Strong(self.question), 
-            footer=None if self.sources == None 
+            Progress() if self.busy else P(self.content, cls="markdown"),
+            header=Strong(self.question),
+            footer=(
+                None
+                if self.sources == None
                 else Details(
-                        Summary("Web links"),
-                        Div(
-                            *[Div(A(search_result['title'], href=search_result['url'])) for search_result in self.sources],
-                            cls='grid'
-                        )
-                ),
-            id=self.id
+                    Summary("Web links"),
+                    Div(
+                        *[
+                            Div(A(search_result["title"], href=search_result["url"]))
+                            for search_result in self.sources
+                        ],
+                        cls="grid",
+                    ),
+                )
+            ),
+            id=self.id,
         )
+
 
 markdown_js = """
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
@@ -67,72 +82,93 @@ import { proc_htmx} from "https://cdn.jsdelivr.net/gh/answerdotai/fasthtml-js/fa
 proc_htmx('.markdown', e => e.innerHTML = marked.parse(e.textContent));
 """
 # FastHTML includes the "HTMX" and "Surreal" libraries in headers, unless you pass `default_hdrs=False`.
-app, rt = fast_app(live=True, # type: ignore
-               # `picolink` is pre-defined with the header for the PicoCSS stylesheet.
-               hdrs=(picolink,
-                     Link(rel='stylesheet', href='https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css', type='text/css'),
-                     Style(""":root { --pico-font-size: 100%;}"""),
-                     Meta(name="color-scheme", content="light dark"),
-                     # MarkdownJS is actually provided as part of FastHTML, but we've included the js code here
-                     # so that you can see how it works.
-                     Script(markdown_js, type='module')
-                ),
-                ws_hdr=True # web socket headers
-            )
+app, rt = fast_app(
+    live=True,  # type: ignore
+    # `picolink` is pre-defined with the header for the PicoCSS stylesheet.
+    hdrs=(
+        picolink,
+        Link(
+            rel="stylesheet",
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css",
+            type="text/css",
+        ),
+        Style(""":root { --pico-font-size: 100%;}"""),
+        Meta(name="color-scheme", content="light dark"),
+        # MarkdownJS is actually provided as part of FastHTML, but we've included the js code here
+        # so that you can see how it works.
+        Script(markdown_js, type="module"),
+    ),
+    ws_hdr=True,  # web socket headers
+)
+
 
 def question_list():
     return Details(
         Summary("Your last 25 questions"),
-        Ul(
-            Li(*chats(order_by='updated DESC', limit=25), dir="ltr"),
-            dir="rtl"
-        ),
+        Ul(Li(*chats(order_by="updated DESC", limit=25), dir="ltr"), dir="rtl"),
         id="question-list",
         cls="dropdown",
-        hx_swap_oob='true')
+        hx_swap_oob="true",
+    )
+
 
 @rt("/")
 def get():
     return RedirectResponse(f"/chat/{new_chatDTO.id}")
 
-@rt('/chat/{id}')
-async def get(id:str):
+
+@rt("/chat/{id}")
+async def get(id: str):
     try:
         if id == new_chatDTO.id:
             chat = new_chatDTO
         else:
             chat = chats[id]
     except:
-        #TODO need to rewrite URL if id != new_ChatDTO.id
+        # TODO need to rewrite URL if id != new_ChatDTO.id
         chat = new_chatDTO
     navigation = Nav(
-        Ul(Li(Hgroup(
-                H3('Be Curious!'),
-                P("There are no stupid questions."))
-        )),
+        Ul(Li(Hgroup(H3("Be Curious!"), P("There are no stupid questions.")))),
         Ul(
-            Li(Button("New question", cls="secondary", onclick="window.location.href='/'")),
+            Li(
+                Button(
+                    "New question", cls="secondary", onclick="window.location.href='/'"
+                )
+            ),
             Li(question_list()),
-           Li(Details(
-                Summary('Theme', role='button', cls='secondary'),
-                Ul(Li(A('Auto', href='#', data_theme_switcher='auto')),
-                    Li(A('Light', href='#', data_theme_switcher='light')),
-                    Li(A('Dark', href='#', data_theme_switcher='dark'))),
-                cls='dropdown')
-            )
-            #Li(A('Login', href='#'))
-        )
+            Li(
+                Details(
+                    Summary("Theme", role="button", cls="secondary"),
+                    Ul(
+                        Li(A("Auto", href="#", data_theme_switcher="auto")),
+                        Li(A("Light", href="#", data_theme_switcher="light")),
+                        Li(A("Dark", href="#", data_theme_switcher="dark")),
+                    ),
+                    cls="dropdown",
+                )
+            ),
+            # Li(A('Login', href='#'))
+        ),
     )
     ask_question = Div(
-        Search(Group(
-            Input(id="new-question", name="question", autofocus=True, placeholder="Ask your question here...", autocomplete="off"), 
-            Button("Answer", id="answer-btn", cls="hidden-default")), 
-            hx_post=f"/chat/{chat.id}", 
-            target_id="answer-list", 
+        Search(
+            Group(
+                Input(
+                    id="new-question",
+                    name="question",
+                    autofocus=True,
+                    placeholder="Ask your question here...",
+                    autocomplete="off",
+                ),
+                Button("Answer", id="answer-btn", cls="hidden-default"),
+            ),
+            hx_post=f"/chat/{chat.id}",
+            target_id="answer-list",
             hx_swap="beforeend",
-            id="search-group"),
+            id="search-group",
+        ),
     )
-    
+
     # restore message histroy for current thread
     checkpoint = get_checkpoint(id)
     if checkpoint != None:
@@ -140,115 +176,148 @@ async def get(id:str):
         content = None
         sources = None
         old_messages = []
-        for msg in checkpoint["channel_values"]['messages']:
+        for msg in checkpoint["channel_values"]["messages"]:
             if isinstance(msg, HumanMessage):
                 if top != None and content != None:
-                    old_messages.append(ChatCard(question=top, content=content, sources=sources))
+                    old_messages.append(
+                        ChatCard(question=top, content=content, sources=sources)
+                    )
                     top, content, sources = None, None, None
                 top = msg.content
             elif isinstance(msg, AIMessage):
-                if 'tool_calls' in msg.additional_kwargs:
+                if "tool_calls" in msg.additional_kwargs:
                     # this is an AIMessage with tool calls. skip
                     continue
                 else:
                     content = msg.content
-            elif isinstance(msg, ToolMessage) and 'results' in msg.artifact:
-                sources = msg.artifact['results'] 
+            elif isinstance(msg, ToolMessage) and "results" in msg.artifact:
+                sources = msg.artifact["results"]
         if top != None and content != None:
-            old_messages.append(ChatCard(question=top, content=content, sources=sources))
+            old_messages.append(
+                ChatCard(question=top, content=content, sources=sources)
+            )
         answer_list = Div(*old_messages, id="answer-list")
     else:
         answer_list = Div(id="answer-list")
-    
+
     body = Body(
-            Container(
-                Header(navigation),
-                Main(answer_list, hx_ext="ws", ws_connect="/ws_connect"),
-                Footer(ask_question)
-            ),
-            Script(src='/static/minimal-theme-switcher.js')
-        )
+        Container(
+            Header(navigation),
+            Main(answer_list, hx_ext="ws", ws_connect="/ws_connect"),
+            Footer(ask_question),
+        ),
+        Script(src="/static/minimal-theme-switcher.js"),
+    )
     return Title("Always be courious."), body
+
 
 # WebSocket connection bookkeeping
 ws_connections = []
-async def on_connect(send): 
+
+
+async def on_connect(send):
     print(f"WS    connect: {send.args[0].client}")
     ws_connections.append(send)
-async def on_disconnect(send): 
-    print(f"WS disconnect: {send.args[0].client}")
-@app.ws('/ws_connect', conn=on_connect, disconn=on_disconnect)
-async def ws(msg:str, send): pass
 
-async def update_chat(card: Card, chat:Any, cleared_inpput, busy_button):
+
+async def on_disconnect(send):
+    print(f"WS disconnect: {send.args[0].client}")
+
+
+@app.ws("/ws_connect", conn=on_connect, disconn=on_disconnect)
+async def ws(msg: str, send):
+    pass
+
+
+async def update_chat(card: Card, chat: Any, cleared_inpput, busy_button):
     inputs = {"messages": [("user", card.question)]}
     config = {"configurable": {"thread_id": chat.id}}
     try:
         result = get_agent("gpt-4o-mini").invoke(inputs, config)
-        #result = get_agent("llama3-groq-8b-8192-tool-use-preview").invoke(inputs, config)
-        #result = get_agent("llama3.1").invoke(inputs, config)
-        if (len(result['messages']) >= 2) and (isinstance(result['messages'][-2], ToolMessage)):
-            tmsg = result['messages'][-2]
-            card.sources = tmsg.artifact['results']
+        # result = get_agent("llama3-groq-8b-8192-tool-use-preview").invoke(inputs, config)
+        # result = get_agent("llama3.1").invoke(inputs, config)
+        if (len(result["messages"]) >= 2) and (
+            isinstance(result["messages"][-2], ToolMessage)
+        ):
+            tmsg = result["messages"][-2]
+            card.sources = tmsg.artifact["results"]
         card.content = result["messages"][-1].content
         chats.upsert(chat)
         success = True
     except BadRequestError as e:
-        #e = "some error"
+        # e = "some error"
         print(f"Exception while calling LLM: {e}")
-        card.content = f"Sorry, due to some technical issue no response could be generated: \n{e}"
+        card.content = (
+            f"Sorry, due to some technical issue no response could be generated: \n{e}"
+        )
         success = False
-    
+
     card.busy = False
     cleared_inpput.disabled = False
     busy_button.disabled = False
     for browser in ws_connections:
-        try: 
+        try:
             await browser(card)
             await browser(cleared_inpput)
             await browser(busy_button)
             if success:
                 await browser(question_list())
             print(f"WS     pushed: {browser.args[0].client}")
-        except Exception as e: 
+        except Exception as e:
             ws_connections.remove(browser)
             print(f"Caught exception: {e}")
             print(f"WS    removed: {browser.args[0].client}")
 
     return success
 
-        
 
 @threaded
-def generate_chat(card: Card, chat:Any, cleared_inpput, busy_button):
+def generate_chat(card: Card, chat: Any, cleared_inpput, busy_button):
     chat.title = card.question if chat.title == None else chat.title
     chat.updated = datetime.now()
     success = asyncio.run(update_chat(card, chat, cleared_inpput, busy_button))
-    if success: 
+    if success:
         global new_chatDTO
         if chat is new_chatDTO:
             new_chatDTO = ChatDTO()
             new_chatDTO.id = shortuuid.uuid()
 
+
 @rt("/chat/{id}")
-async def post(question:str, id:str):
+async def post(question: str, id: str):
     try:
         if id == new_chatDTO.id:
             chat = new_chatDTO
         else:
             chat = chats[id]
     except:
-        #TODO need to rewrite URL if id != new_ChatDTO.id
+        # TODO need to rewrite URL if id != new_ChatDTO.id
         chat = new_chatDTO
-    card = ChatCard(question=question, content='', busy=True)
-    cleared_inpput = Input(id="new-question", name="question", autofocus=True, placeholder="Ask your question here...", disabled=True, hx_swap_oob='true')
-    busy_button = Button("Answer", id="answer-btn", cls="hidden-default", disabled=True, hx_swap_oob='true')
+    card = ChatCard(question=question, content="", busy=True)
+    cleared_inpput = Input(
+        id="new-question",
+        name="question",
+        autofocus=True,
+        placeholder="Ask your question here...",
+        autocomplete="off",
+        disabled=True,
+        hx_swap_oob="true",
+    )
+    busy_button = Button(
+        "Answer",
+        id="answer-btn",
+        cls="hidden-default",
+        disabled=True,
+        hx_swap_oob="true",
+    )
     generate_chat(card, chat, cleared_inpput, busy_button)
     return card, cleared_inpput, busy_button
 
+
 def main():
-    print('preparing html server')
+    print("preparing html server")
     serve()
+
 
 if __name__ == "__main__":
     main()
